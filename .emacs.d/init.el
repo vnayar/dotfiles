98469047cf6a5a64d17bb3dc28000f800ed3f1a7
Vijay Nayar <madric@gmail.com>
2016-05-26 14:35:09 -0400

    Added logic to assure required packages are installed.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,33 @@
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+  (defun ensure-package-installed (&rest packages)
+    "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+    (mapcar
+     (lambda (package)
+       (if (package-installed-p package)
+       nil
+     (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+         (package-install package)
+       package)))
+     packages))
+
+  ;; make sure to have downloaded archive description.
+  ;; Or use package-archive-contents as suggested by Nicolas Dudebout
+  (or (file-exists-p package-user-dir)
+      (package-refresh-contents))
+
+  (ensure-package-installed
+   'bash-completion
+   'd-mode
+   'ggtags
+   'markdown-mode
+   'plantuml-mode
+   'polymode
+   'top-mode)
   )
 
 ;; Theme Settings
